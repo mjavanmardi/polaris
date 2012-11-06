956ff7f01f9191f006eb167bfd3474ff642a95ab
Bo Xu <bxu@anl.gov>
2012-11-06 23:29:05 +0000

    porting to v0.3 release

--- User_Space/User_Space.h
+++ User_Space/User_Space.h
@@ -1,6 +1,15 @@
 #pragma once
 
 #include "User_Space_Includes.h"
+//#include "Signal_Implementations.h"
+#include "Polaris_Intersection_Implementation.h"
+#include "Polaris_Link_Implementation.h"
+#include "Polaris_Network_Implementation.h"
+#include "Polaris_Vehicle_Implementation.h"
+#include "Polaris_Routing_Implementation.h"
+#include "Polaris_Demand_Implementation.h"
+#include "Polaris_Activity_Location_Implementation.h"
+#include "Polaris_Traveler_Implementation.h"
 
 
 
