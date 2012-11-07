2b7ca47fb1ea2a01a9da08ace118a759fc4bb148
Bo Xu <bxu@anl.gov>
2012-11-07 18:47:23 +0000

    scenario support

---
+++ Scenario_Manager/cfg_reader.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "json.h"
+
+#define PARAMETER_NOT_SPECIFIED 1
+#define PARAMETER_TYPE_NOT_MATCH 2
+
+typedef std::vector<int> IntArray;
+typedef std::vector<double> DoubleArray;
+typedef std::vector<std::string> StringArray;
+typedef std::vector<bool> BoolArray;
+
+class CfgReader
+{
+	Json::Value root;
+	bool parseValueTree( const std::string &input );
+	Json::Value getParameter( const char *parameterName );
+public:
+	bool initialize( const char *path );
+	int getParameter( const char *parameterName, int *paramValue );
+	int getParameter( const char *parameterName, double *paramValue );
+	int getParameter( const char *parameterName, std::string *paramValue );
+	int getParameter( const char *parameterName, bool *paramValue );
+	int getParameter( const char *parameterName, IntArray *parameter);
+	int getParameter( const char *parameterName, DoubleArray *parameter);
+	int getParameter( const char *parameterName, StringArray *parameter);
+	int getParameter( const char *parameterName, BoolArray *parameter);
+};
+
