778c8b69b19175e80892a4da73f5974cf64d5cc4
Bo Xu <bxu@anl.gov>
2013-01-16 21:44:42 +0000

    change thread number to 1 page size to 4096

--- Core/Run_Parameters.h
+++ Core/Run_Parameters.h
@@ -1,17 +1,16 @@
 #pragma once
 #include "Polaris_Variables.h"
 
-
 static const long long _Megabytes_To_Allocate=2000;
 static const unsigned int _num_iterations=3600*24*2; // 2 days
-static const int _num_threads=4;
+static const int _num_threads=1;
 
 static const int _Max_Message_Size=5*1048576;
 
-static const long long _Page_Size=4096/8;
+static const long long _Page_Size=4096;
 static const long long _Max_Pages=(_Megabytes_To_Allocate*1048576)/_Page_Size;
 
-typedef Basic_Units::Time_Variables::Time_Minutes Simulation_Timestep_Increment;
+typedef Basic_Units::Time_Variables::Time_Seconds Simulation_Timestep_Increment;
 
 
 #define STATE_CHECKS 0
