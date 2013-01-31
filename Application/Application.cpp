f5da7654d7d77b3d96fc4679b224c7d4e248d489
Michael Hope <hope@anl.gov>
2013-01-31 21:32:33 +0000

    Reference removed to Antares in Release mode - Application.cpp corrected

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -4,44 +4,6 @@
 
 #include "Application_Includes.h"
 
-
-//implementation struct Network
-//{
-//	feature_implementation void Do_Stuff(int a, requires(check(TargetType, is_arithmetic)));
-//	//feature_implementation void Do_Stuff(int a);
-//};
-//
-//
-//template<typename MasterType,typename ParentType>
-//template<typename ComponentType, typename CallerType, typename TargetType>
-//feature_implementation_definition
-//void Network<MasterType,ParentType>::Do_Stuff(int a, requires(check(TargetType, is_arithmetic)))
-//{
-//	cout << "hooray" << endl;
-//}
-
-
-
-template<typename MyType>
-struct Network
-{
-	template<typename MyOtherType>
-	void Do_Stuff(char param[MyOtherType::size]);
-};
-
-template<typename MyType>
-template<typename MyOtherType>
-void Network<MyType>::Do_Stuff(char param[MyOtherType::size])
-{
-};
-
-struct Array_Size
-{
-	static const int size;
-};
-
-const int Array_Size::size=3;
-
 int main()
 {
 }
