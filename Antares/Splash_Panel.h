80aa0186caff88d005e593a219468dee18cdb188
Michael Hope <hope@anl.gov>
2013-05-01 19:54:08 +0000

    Bug Fix - adding splash panel

---
+++ Antares/Splash_Panel.h
@@ -0,0 +1,15 @@
+//*********************************************************
+//	Splash_Panel.h - Plotting Canvas
+//*********************************************************
+
+#pragma once
+#include "Antares_Includes.h"
+
+//---------------------------------------------------------
+//	Splash_Panel - information page class definition
+//---------------------------------------------------------
+
+prototype struct Splash_Panel
+{
+	tag_as_prototype;
+};
