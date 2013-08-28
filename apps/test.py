342e9b9c9d44addda6cb4249871c20235c07888b
Vadim Sokolov <vsokolov@anl.gov>
2013-08-28 21:27:33 +0000

    Adding POLARIS Aanalyzer tool. The goal of the tool is to generate aggregate plots and tables to be used for reporting and model validation/calibration

---
+++ apps/test.py
@@ -0,0 +1,9 @@
+from PyQt4 import QtCore, QtGui
+from polaris_dialog import PolarisDialog
+if __name__ == '__main__':
+    import sys
+
+    app = QtGui.QApplication(sys.argv)
+    window = PolarisDialog()
+    window.show()
+    sys.exit(app.exec_())
