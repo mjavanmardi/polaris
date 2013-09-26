bc230f72a85b923e9fb6311ac6e0d4661b84b5f8
MichaelHope <hope@anl.gov>
2013-09-26 11:07:31 -0500

    Added local_concepts for developing quick checks within a class

--- polaris_git/Libs/Core/Local_Concepts.h
+++ polaris_git/Libs/Core/Local_Concepts.h
@@ -1,21 +1,24 @@
 #pragma once
 ///----------------------------------------------------------------------------------------------------
-/// Requirements.h - Class Level Requirement Checking Library on "ComponentType"
+/// Local_Concepts.h - Local concept checks
 ///----------------------------------------------------------------------------------------------------
 
-#include "Local_Concepts.h"
+#include "Concepts.h"
 
 namespace polaris
 {
 	///----------------------------------------------------------------------------------------------------
-	/// requires_general_check - name must meet user-defined compile time check
+	/// Concept Checks
 	///----------------------------------------------------------------------------------------------------
 
-	#define requires_general_check(CHECK_ALIAS,NAME,...)\
-	union\
-	{\
+
+	///----------------------------------------------------------------------------------------------------
+	/// general_check - user-defined check
+	///----------------------------------------------------------------------------------------------------
+
+	#define local_general_check(CHECK_ALIAS,...)\
 	template<typename T>\
-	struct CHECK_ALIAS##_Requires_General_Check\
+	struct CHECK_ALIAS\
 	{\
 		template<typename U,bool Perform_Check = !is_same<U,NULLTYPE>::value>\
 		struct data_check{ static const bool value = true; };\
@@ -24,20 +27,15 @@ namespace polaris
 		struct data_check<U,true>{ static const bool value = __VA_ARGS__; };\
 		\
 		static const bool value = data_check<T>::value;\
-	};\
-	\
-	static_assert(CHECK_ALIAS##_Requires_General_Check<ComponentType>::value,"\n\n\n[--------- Requirement Named:   \"" #NAME "\"  Did Not Meet the Check:   \"" #__VA_ARGS__ "\"! ---------]\n\n");\
 	};
 
 	///----------------------------------------------------------------------------------------------------
-	/// requires_typedef_name - typedef of given name must exist
+	/// check_typedef_name - typedef of given name must exist
 	///----------------------------------------------------------------------------------------------------
 
-	#define requires_typedef_name(CHECK_ALIAS,NAME)\
-	union\
-	{\
+	#define local_check_typedef_name(CHECK_ALIAS,NAME)\
 	template<typename T>\
-	struct CHECK_ALIAS##_Requires_Type_Defined\
+	struct CHECK_ALIAS\
 	{\
 		template<typename U,bool Perform_Check = !is_same<U,NULLTYPE>::value>\
 		struct data_check{ static const bool value = true; };\
@@ -58,20 +56,15 @@ namespace polaris
 		};\
 		\
 		static const bool value = data_check<T>::value;\
-	};\
-	\
-	static_assert(CHECK_ALIAS##_Requires_Type_Defined<ComponentType>::value,"\n\n\n[--------- Typename Requirement Named:   \"" #NAME "\"   Was Not Found! ---------]\n\n");\
 	};
 
 	///----------------------------------------------------------------------------------------------------
-	/// requires_typedef_type - typedef of given name must exist and match given type
+	/// check_typedef_type - typedef of given name must exist and match given type
 	///----------------------------------------------------------------------------------------------------
 
-	#define requires_typedef_type(CHECK_ALIAS,NAME,TYPE)\
-	union\
-	{\
+	#define local_check_typedef_type(CHECK_ALIAS,NAME,TYPE)\
 	template<typename T>\
-	struct CHECK_ALIAS##_Requires_Type\
+	struct CHECK_ALIAS\
 	{\
 		template<typename U,bool Perform_Check = !is_same<U,NULLTYPE>::value>\
 		struct data_check{ static const bool value = true; };\
@@ -92,20 +85,15 @@ namespace polaris
 		};\
 		\
 		static const bool value = data_check<T>::value;\
-	};\
-	\
-	static_assert(CHECK_ALIAS##_Requires_Type<ComponentType>::value,"\n\n\n[--------- Typename Requirement of Type:   " #TYPE "    Named:   \"" #NAME "\"   Was Not Found! ---------]\n\n");\
 	};
 
 	///----------------------------------------------------------------------------------------------------
-	/// requires_data_member_name - given name must be a data member
+	/// check_data_member_name - given name must be a data member
 	///----------------------------------------------------------------------------------------------------
 
-	#define requires_data_member_name(CHECK_ALIAS,NAME)\
-	union\
-	{\
+	#define local_check_data_member_name(CHECK_ALIAS,NAME)\
 	template<typename T>\
-	struct CHECK_ALIAS##_Requires_Named_Data\
+	struct CHECK_ALIAS\
 	{\
 		template<typename U,bool Perform_Check = !is_same<U,NULLTYPE>::value>\
 		struct data_check{ static const bool value = true; };\
@@ -126,20 +114,15 @@ namespace polaris
 		};\
 		\
 		static const bool value = data_check<T>::value;\
-	};\
-	\
-	static_assert(CHECK_ALIAS##_Requires_Named_Data<ComponentType>::value,"\n\n\n[--------- Data Requirement Named:   \"" #NAME "\"   Was Not Found! ---------]\n\n");\
 	};
 
 	///----------------------------------------------------------------------------------------------------
-	/// requires_data_member_type - given name must be a data member and match given type
+	/// check_data_member_type - given name must be a data member and match given type
 	///----------------------------------------------------------------------------------------------------
 
-	#define requires_data_member_type(CHECK_ALIAS,NAME,TYPE)\
-	union\
-	{\
+	#define local_check_data_member_type(CHECK_ALIAS,NAME,TYPE)\
 	template<typename T>\
-	struct CHECK_ALIAS##_Requires_Typed_Data\
+	struct CHECK_ALIAS\
 	{\
 		template<typename U,bool Perform_Check = !is_same<U,NULLTYPE>::value>\
 		struct data_check{ static const bool value = true; };\
@@ -160,20 +143,15 @@ namespace polaris
 		};\
 		\
 		static const bool value = data_check<T>::value;\
-	};\
-	\
-	static_assert(CHECK_ALIAS##_Requires_Typed_Data<ComponentType>::value,"\n\n\n[--------- Data Requirement of Type:   " #TYPE "    Named:   \"" #NAME "\"   Was Not Found! ---------]\n\n");\
 	};
 
 	///----------------------------------------------------------------------------------------------------
-	/// requires_data_member_meets_check - given name must be a data member and meet user defined compile time check
+	/// check_data_member_meets_check - given name must be a data member and meet user defined compile time check
 	///----------------------------------------------------------------------------------------------------
 
-	#define requires_data_member_meets_check(CHECK_ALIAS,NAME,...)\
-	union\
-	{\
+	#define local_check_data_member_meets_check(CHECK_ALIAS,NAME,...)\
 	template<typename T>\
-	struct CHECK_ALIAS##_Requires_Data_Meets_Check\
+	struct CHECK_ALIAS\
 	{\
 		template<typename U,bool Perform_Check = !is_same<U,NULLTYPE>::value>\
 		struct data_check{ static const bool value = true; };\
@@ -194,20 +172,15 @@ namespace polaris
 		};\
 		\
 		static const bool value = data_check<T>::value;\
-	};\
-	\
-	static_assert(CHECK_ALIAS##_Require_Data_Meets_Check<ComponentType>::value,"\n\n\n[--------- Data Requirement Named:   \"" #NAME "\"  Either Does Not Exist or Did Not Meet the Check:   \"" #__VA_ARGS__ "\"! ---------]\n\n");\
 	};
 
 	///----------------------------------------------------------------------------------------------------
-	/// requires_method_name - given name must be a member method
+	/// check_method_name - given name must be a member method
 	///----------------------------------------------------------------------------------------------------
 
-	#define requires_method_name(CHECK_ALIAS,NAME)\
-	union\
-	{\
+	#define local_check_method_name(CHECK_ALIAS,NAME)\
 	template<typename T>\
-	struct CHECK_ALIAS##_Requires_Named_Method_Checker\
+	struct CHECK_ALIAS\
 	{\
 		template<typename U,bool Perform_Check = !is_same<U,NULLTYPE>::value>\
 		struct function_check{ static const bool value = true; };\
@@ -228,20 +201,15 @@ namespace polaris
 		};\
 		\
 		static const bool value = function_check<T>::value;\
-	};\
-	\
-	static_assert(CHECK_ALIAS##_Requires_Named_Method_Checker<ComponentType>::value,"\n\n\n[--------- Method Requirement Named:   \"" #NAME "\"   Was Not Found! ---------]\n\n");\
 	};
 
 	///----------------------------------------------------------------------------------------------------
-	/// requires_method_type - given name must be a method member and match given signature
+	/// check_method_type - given name must be a method member and match given signature
 	///----------------------------------------------------------------------------------------------------
 
-	#define requires_method_type(CHECK_ALIAS,NAME,FUNCTION_SIGNATURE)\
-	union\
-	{\
+	#define local_check_method_type(CHECK_ALIAS,NAME,FUNCTION_SIGNATURE)\
 	template<typename T>\
-	struct CHECK_ALIAS##_Requires_Typed_Method_Checker\
+	struct CHECK_ALIAS\
 	{\
 		template<typename U,bool Perform_Check = !is_same<U,NULLTYPE>::value>\
 		struct function_check{ static const bool value = true; };\
@@ -262,79 +230,66 @@ namespace polaris
 		};\
 		\
 		static const bool value = function_check<T>::value;\
-	};\
-	\
-	static_assert(CHECK_ALIAS##_Requires_Typed_Method_Checker<ComponentType>::value,"\n\n\n[--------- Method Requirement of Type:   " #FUNCTION_SIGNATURE "    Named:   \"" #NAME "\"   Was Not Found! ---------]\n\n");\
 	};
 
 	///----------------------------------------------------------------------------------------------------
-	/// requires_template_method_name - given name must be a method template member
+	/// check_template_method_name - given name must be a method template member
 	///----------------------------------------------------------------------------------------------------
 
-	#define requires_template_method_name(CHECK_ALIAS,NAME)\
-		union\
+	#define local_check_template_method_name(CHECK_ALIAS,NAME)\
+	template<typename T>\
+	struct CHECK_ALIAS\
+	{\
+		template<typename U,bool Perform_Check = !is_same<U,NULLTYPE>::value>\
+		struct function_check{ static const bool value = true; };\
+		template<typename U>\
+		struct function_check<U,true>\
 		{\
-			template<typename T>\
-			struct CHECK_ALIAS##_Requires_Typed_Method_Checker\
-			{\
-				template<typename U,bool Perform_Check = !is_same<U,NULLTYPE>::value>\
-				struct function_check{ static const bool value = true; };\
-				template<typename U>\
-				struct function_check<U,true>\
-				{\
-					template<typename V> static small_type has_matching_named_member(void (V::* arg)() = &V::NAME<NULLTYPE>);\
-					template<typename V> static large_type has_matching_named_member(...);\
-					\
-					template<typename V,bool Perform_Check = (sizeof(has_matching_named_member<U>(nullptr))==success)>\
-					struct form_check{ static const bool value = false; };\
-					\
-					template<typename V>\
-					struct form_check<V,true>{ static const int value = true;};\
-					\
-					static const bool value = form_check<U>::value;\
-				};\
-				\
-				static const bool value = function_check<T>::value;\
-			};\
+			template<typename V> static small_type has_matching_named_member(void (V::* arg)() = &V::NAME<NULLTYPE>);\
+			template<typename V> static large_type has_matching_named_member(...);\
 			\
-			static_assert(CHECK_ALIAS##_Requires_Typed_Method_Checker<ComponentType>::value,"\n\n\n[--------- Template Method Requirement Named:   \"" #NAME "\" Was Not Found! ---------]\n\n");\
+			template<typename V,bool Perform_Check = (sizeof(has_matching_named_member<U>(nullptr))==success)>\
+			struct form_check{ static const bool value = false; };\
+			\
+			template<typename V>\
+			struct form_check<V,true>{ static const int value = true;};\
+			\
+			static const bool value = form_check<U>::value;\
 		};\
+		\
+		static const bool value = function_check<T>::value;\
+	};
 
 	///----------------------------------------------------------------------------------------------------
-	/// requires_template_method_type - given name must be a method template member and match signature
+	/// check_template_method_type - given name must be a method template member and match signature
 	///----------------------------------------------------------------------------------------------------
 
-	#define requires_template_method_type(CHECK_ALIAS,NAME,FUNCTION_SIGNATURE,...)\
-		union\
+	#define local_check_template_method_type(CHECK_ALIAS,NAME,FUNCTION_SIGNATURE,...)\
+	template<typename T>\
+	struct CHECK_ALIAS\
+	{\
+		template<typename U,bool Perform_Check = !is_same<U,NULLTYPE>::value>\
+		struct function_check{ static const bool value = true; };\
+		template<typename U>\
+		struct function_check<U,true>\
 		{\
-			template<typename T>\
-			struct CHECK_ALIAS##_Requires_Typed_Method_Checker\
-			{\
-				template<typename U,bool Perform_Check = !is_same<U,NULLTYPE>::value>\
-				struct function_check{ static const bool value = true; };\
-				template<typename U>\
-				struct function_check<U,true>\
-				{\
-					template<typename V> static small_type has_matching_named_member(void (V::* arg)() = &V::NAME<__VA_ARGS__>);\
-					template<typename V> static large_type has_matching_named_member(...);\
-					\
-					template<typename V,bool Perform_Check = (sizeof(has_matching_named_member<U>(nullptr))==success)>\
-					struct form_check{ static const bool value = false; };\
-					\
-					template<typename V> static small_type has_matching_formed_member(typename FUNCTION_SIGNATURE::function_type);\
-					template<typename V> static large_type has_matching_formed_member(...);\
-					\
-					template<typename V>\
-					struct form_check<V,true>{ static const int value = (sizeof(has_matching_formed_member<V>(&V::NAME<__VA_ARGS__>))==success);};\
-					\
-					static const bool value = form_check<U>::value;\
-				};\
-				\
-				static const bool value = function_check<T>::value;\
-			};\
+			template<typename V> static small_type has_matching_named_member(void (V::* arg)() = &V::NAME<__VA_ARGS__>);\
+			template<typename V> static large_type has_matching_named_member(...);\
 			\
-			static_assert(CHECK_ALIAS##_Requires_Typed_Method_Checker<ComponentType>::value,"\n\n\n[--------- Template Method Requirement of Type:   " #FUNCTION_SIGNATURE "    Named:   \"" #NAME "\"   With Arguments \"" #__VA_ARGS__ "\" Was Not Found! ---------]\n\n");\
+			template<typename V,bool Perform_Check = (sizeof(has_matching_named_member<U>(nullptr))==success)>\
+			struct form_check{ static const bool value = false; };\
+			\
+			template<typename V> static small_type has_matching_formed_member(typename FUNCTION_SIGNATURE::function_type);\
+			template<typename V> static large_type has_matching_formed_member(...);\
+			\
+			template<typename V>\
+			struct form_check<V,true>{ static const int value = (sizeof(has_matching_formed_member<V>(&V::NAME<__VA_ARGS__>))==success);};\
+			\
+			static const bool value = form_check<U>::value;\
 		};\
+		\
+		static const bool value = function_check<T>::value;\
+	};
 
 
 }
