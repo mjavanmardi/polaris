9eb821b7298cdc7a2b29306b1cc71a2c89e15b64
josh auld <jauld@anl.gov>
2015-09-29 15:08:13 -0500

    Added linkage to previous study runs for iterative analysis

--- bin/scenario_manager_light/run_scenarios.py
+++ bin/scenario_manager_light/run_scenarios.py
@@ -32,22 +32,33 @@ if not os.path.exists(study_folder):
 polaris_exe = s_doc['polaris_exe']
 polaris_exe_basename = os.path.basename(polaris_exe)
 bin_folder = study_folder + '/bin'
+
+prev_study_folder = ""
+if 'previous_run' in s_doc:
+	prev_study_folder = s_doc['previous_run']	
+	if not os.path.exists(prev_study_folder):
+		print "ERROR: previous study run directory not found."
+		sys.exit()
+
 if not os.path.exists(bin_folder):
 	os.makedirs(bin_folder)	
 shutil.copy(polaris_exe, bin_folder)
 batch_fh = open(study_folder + '/run.bat','w')
 scenario_id = 1
-for scenario in s_doc['scnarios']:
+for scenario in s_doc['scenarios']:
 	print scenario_id
 	m_doc = json.load(open(master_scenario_file))
 	for parmeter_key in scenario:
 		m_doc[parmeter_key] = scenario[parmeter_key]
 	m_doc['output_dir_name'] = bath_id+'_'+str(scenario_id)
 	m_doc['master_scenario_file'] = master_scenario_file
+	
+	if prev_study_folder <> "":
+		m_doc['input_highway_skim_file_path_name'] = prev_study_folder + '/' + bath_id+'_'+str(scenario_id) + '/' + m_doc['input_highway_skim_file_path_name']
 	scenario_file_name = 'scenario_'+str(scenario_id)+'.json'
 	with open(study_folder + '/' +  scenario_file_name,'w') as fh:
 		fh.write(json.dumps(m_doc, sort_keys=True, indent=4, separators=(',',': ')))
-	batch_fh.write(".\\bin\\%s %s 16\n"%(polaris_exe_basename, scenario_file_name))
+	batch_fh.write(".\\bin\\%s %s 22\n"%(polaris_exe_basename, scenario_file_name))
 	scenario_id+=1	
 	
 	
