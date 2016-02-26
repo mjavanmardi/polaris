ee210bc177d34b73fda9e3f3c7c30c334b3899d1
unknown <vsokolov@CSI351791.es.anl.gov>
2016-02-26 16:34:26 -0600

    Cleaned legacy folders. Removed capacity based flow model and gtest

--- libs/flow_lib/toqueues.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "road.h"
-
-void travelingAreaToQueues(std::map<int, Road>& Roads, int timestep);
-
-std::pair<double,double> newCarDistanceAndSpeed(double carSpeed, double speedLimit, int timestep, double distanceLeftToTravel, double accMean, double deccMean);
