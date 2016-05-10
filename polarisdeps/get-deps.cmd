04d8cdee6bea1452337342d60eab1a30d924079e
Randy Weimer <rweimer@anl.gov>
2016-05-10 14:45:49 -0500

    Dependency build fixes

--- polarisdeps/get-deps.cmd
+++ polarisdeps/get-deps.cmd
@@ -1,11 +1,33 @@
-@ECHO OFF
+rem @ECHO OFF
 
 IF NOT "%1" == "" (
 	set BASEDIR=%1
+	call polaris_env.bat %BASEDIR%
 ) ELSE IF NOT "%POLARIS_DEPS_DIR%" == "" (
-	  set BASEDIR=%POLARIS_DEPS_DIR%
+	set BASEDIR=%POLARIS_DEPS_DIR%
 ) ELSE (
 	set BASEDIR=%CD%
+	call polaris_env.bat %BASEDIR%
+)
+
+echo %BASEDIR%
+COPY polaris_env.bat %BASEDIR%
+
+REM  see if Vsual Studio is already set
+SET VCROOT=
+IF "%VSINSTALLDIR%" == "" (
+	call find_msvc.cmd 14
+    IF ERRORLEVEL 1 exit /B %ERRORLEVEL%
+) ELSE IF NOT "%VisualStudioVersion%" == "14.0" (
+	echo "Visual Studio 14.0 (2015) is required"
+	exit /B 1
+) ELSE IF NOT "%Platform%" == "X64" (
+	echo "Visual Studio 14.0 (2015) must be set for X64 platform"
+	exit /B 1
+)
+
+IF NOT "%VCROOT%" == "" (
+    call "%VCROOT%\vcvarsall.bat" amd64
 )
 
 :: get our slashes all straightened out :)
