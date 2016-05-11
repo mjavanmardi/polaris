14a778b6117e8c13eb050a082310a227df861a4c
Randy Weimer <rweimer@anl.gov>
2016-05-11 12:52:51 -0500

    Dependency build script changes to locate a proper Python install

--- polarisdeps/build-boost-1.60.0.cmd
+++ polarisdeps/build-boost-1.60.0.cmd
@@ -21,17 +21,20 @@ set BOOSTDIR=%BASEDIR%\boost_1_60_0
 echo file=%BOOSTZIPFILE%
 echo dir=%BOOSTDIR%
 
-myWget.py -u "http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.zip" -n %BOOSTZIPFILE% -e %BOOSTDIR% -o %BASEDIR%
+call find_python.cmd
+IF "%MYPYTHONPATH%" == "" ( ECHO "Can't find python" & EXIT /B 1)
+
+%MYPYTHONPATH% myWget.py -u "http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.zip" -n %BOOSTZIPFILE% -e %BOOSTDIR% -o %BASEDIR%
 
 
 :: if you want to use boost libraries (as opposed to just headers)
 :: uncomment the commands here:
 
-::cd /D %BOOSTDIR%
+::cd %BOOSTDIR%
 ::call bootstrap.bat
 ::b2 address-model=64 link=shared,static variant=release install --prefix=%BOOSTDIR%
 ::b2 address-model=64 link=shared,static variant=debug install --prefix=%BOOSTDIR%
-::cd /D %BOOSTDIR%
+::cd %BOOSTDIR%
 ::ren lib lib64-msvc-14.0
 
-cd /D %~dp0
+cd %~dp0
--- polarisdeps/build-odb-2.4.0.cmd
+++ polarisdeps/build-odb-2.4.0.cmd
@@ -36,10 +36,14 @@ echo SQLITE3DIR=        %SQLITE3DIR%
 echo filedir=           %~dp0
 
 :: GOTO END
-myWget.py -u "http://www.codesynthesis.com/download/odb/2.4/odb-2.4.0-i686-windows.zip" -n %ODBZIPFILE% -e %ODBDIR% -o %BASEDIR%
-myWget.py -u "http://www.codesynthesis.com/download/odb/2.4/libodb-2.4.0.zip" -n %LIBODBZIPFILE% -e %LIBODBDIR% -o %BASEDIR%
-myWget.py -u "http://www.codesynthesis.com/download/odb/2.4/libodb-sqlite-2.4.0.zip" -n %ODBSQLITEBZIPFILE% -e %LIBSQLITEDIR% -o %LIBODBDIR%
-myWget.py -u "https://www.sqlite.org/2016/sqlite-amalgamation-3110100.zip" -n %SQLITEZIPFILE% -e %SQLITE3DIR% -o %LIBODBDIR%
+
+call find_python.cmd
+IF "%MYPYTHONPATH%" == "" ( ECHO "Can't find python" & EXIT /B 1)
+
+%MYPYTHONPATH% myWget.py -u "http://www.codesynthesis.com/download/odb/2.4/odb-2.4.0-i686-windows.zip" -n %ODBZIPFILE% -e %ODBDIR% -o %BASEDIR%
+%MYPYTHONPATH% myWget.py -u "http://www.codesynthesis.com/download/odb/2.4/libodb-2.4.0.zip" -n %LIBODBZIPFILE% -e %LIBODBDIR% -o %BASEDIR%
+%MYPYTHONPATH% myWget.py -u "http://www.codesynthesis.com/download/odb/2.4/libodb-sqlite-2.4.0.zip" -n %ODBSQLITEBZIPFILE% -e %LIBSQLITEDIR% -o %LIBODBDIR%
+%MYPYTHONPATH% myWget.py -u "https://www.sqlite.org/2016/sqlite-amalgamation-3110100.zip" -n %SQLITEZIPFILE% -e %SQLITE3DIR% -o %LIBODBDIR%
 
 mkdir %LIBODBDIR%
 echo copy %~dp0libodb-vc14.sln %LIBODBDIR%
