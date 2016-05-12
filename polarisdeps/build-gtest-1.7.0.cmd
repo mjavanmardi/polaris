2a25ee3d8691be0326d98fa1a395254f321835af
Randy Weimer <rweimer@anl.gov>
2016-05-12 16:42:03 -0500

    Add checks in build gtest so it can run independently

--- polarisdeps/build-gtest-1.7.0.cmd
+++ polarisdeps/build-gtest-1.7.0.cmd
@@ -14,6 +14,26 @@ set a=%a:/=\%
 echo %a%
 set BASEDIR=%a%
 
+REM  see if Vsual Studio is already set
+SET VCROOT=
+IF "%VSINSTALLDIR%" == "" (
+	call find_msvc.bat 14
+    IF ERRORLEVEL 1 exit /B %ERRORLEVEL%
+) ELSE IF NOT "%VisualStudioVersion%" == "14.0" (
+	echo "Visual Studio 14.0 (2015) is required"
+	exit /B 1
+) ELSE IF NOT "%Platform%" == "X64" (
+	echo "Visual Studio 14.0 (2015) must be set for X64 platform"
+	exit /B 1
+)
+
+IF NOT "%VCROOT%" == "" (
+    call "%VCROOT%\vcvarsall.bat" amd64
+)
+
+:: cal this because for some goofy reason it fails on the first call - but then works
+call find_python.cmd
+
 :: Download and expand source files
 set GTESTZIPFILE=release-1.7.0.zip
 set GTESTDIR=%BASEDIR%\googletest-release-1.7.0
