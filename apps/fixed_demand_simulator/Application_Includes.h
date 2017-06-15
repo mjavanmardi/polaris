d984eaa320dff4610ed0f3fa83cecd67bccf09c1
josh auld <jauld@anl.gov>
2017-06-15 10:34:48 -0500

    Fixed bug in fixed_demand_simulator for using new library structure

--- apps/fixed_demand_simulator/Application_Includes.h
+++ apps/fixed_demand_simulator/Application_Includes.h
@@ -7,8 +7,10 @@
 #include "Visual_Traffic_Simulator.h"
 #else
 
-#include "traffic_simulator/User_Space_with_odb.h"
-#include "traffic_simulator/User_Space.h"
+#include "traffic_simulator/Traffic_Simulator.h"
+#include "its_simulator/ITS_Simulator.h"
+#include "scenario_manager/Scenario_Manager.h"
+
 
 #endif
 
