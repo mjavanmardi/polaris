15260b8ed29683b3937e9ab53d983c8e4eb99f37
josh auld <jauld@anl.gov>
2018-03-26 23:02:27 -0500

    Minor bug fix

--- apps/batch_router/Batch_Router_Implementation.h
+++ apps/batch_router/Batch_Router_Implementation.h
@@ -173,6 +173,7 @@ namespace Batch_Router_Components
 					}					
 					else
 					{
+						summary_by_thread[__thread_id] << _ID << "\t" << __thread_id << "\t\t\t\t\t\t" << movement->routed_travel_time<float>()<<endl;
 						results_by_thread[__thread_id] << movement->routed_travel_time<float>() << ": ";
 
 						_Trajectory_Container_Interface* trajectory = movement->trajectory_container<_Trajectory_Container_Interface*>();
@@ -194,6 +195,7 @@ namespace Batch_Router_Components
 					}
 					else
 					{
+						summary_by_thread[__thread_id] << _ID << "\t" << __thread_id << "\t" << "fail" << endl;
 						results_by_thread[__thread_id] << _ID << ": Error, movement was not routable." << endl;
 					}
 					
